9618ac80b200bb716f013592fcb32f570b81ab37
Piotr Solnica <piotr.solnica@gmail.com>
2021-04-21 12:15:33 +0200

    [rubocop] update entrypoint

--- action-rubocop/entrypoint.sh
+++ action-rubocop/entrypoint.sh
@@ -3,7 +3,7 @@
 result_file="result-rubocop-$GITHUB_RUN_ID.json"
 
 echo "::debug::rubocop start"
-dt metrics rubocop --root $GITHUB_WORKSPACE $1 -- -o "$result_file"
-grep '::debug::' /cli/log/cli.log
+ah metrics rubocop --root $GITHUB_WORKSPACE $1 -- -o "$result_file"
+grep '::debug::' /usr/local/src/cli/log/cli.log
 echo "::debug::rubocop stop"
 echo "::set-output name=result_file::$result_file"
