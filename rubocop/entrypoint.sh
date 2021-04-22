bf4cb938700ccdf2d762478614107598e9167c6b
Piotr Solnica <piotr.solnica@gmail.com>
2021-04-22 18:41:46 +0200

    [rubocop] update workflow

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-result_file="result-rubocop-$GITHUB_RUN_ID.json"
+result_file="$GITHUB_WORKSPACE/ah-result-$GITHUB_RUN_ID.json"
 
 echo "::debug::rubocop start"
-ah metrics rubocop --root $GITHUB_WORKSPACE -- -o "$result_file" $1
+ah metrics rubocop --root $GITHUB_WORKSPACE -- --format json --out $result_file $1
 grep '::debug::' /usr/local/src/cli/log/cli.log
 echo "::debug::rubocop stop"
 echo "::set-output name=result_file::$result_file"
