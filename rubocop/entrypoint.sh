85aa208ba73526dd8f74461d8f682a17841c05d1
Piotr Solnica <piotr.solnica@gmail.com>
2021-04-22 19:22:42 +0200

    [rubocop] update workflow

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh -l
 
-result_file="/tmp/ah-result-$GITHUB_RUN_ID.json"
-
-echo $(pwd)
+result_file="ah-result-$GITHUB_RUN_ID.json"
 
 ah metrics rubocop --root $GITHUB_WORKSPACE -- --format json --out $result_file $*
-
-echo $(cat "/var/log/ah-cli.log")
-
-echo $(cat $result_file)
 
 echo "::set-output name=result_file::$result_file"
