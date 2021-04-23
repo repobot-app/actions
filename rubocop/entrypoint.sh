73918710231c42bc2b187a23c065f6c4cd2446e7
Piotr Solnica <piotr.solnica@gmail.com>
2021-04-23 19:04:28 +0200

    wip

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -3,7 +3,7 @@
 if [[ $GITHUB_EVENT_NAME == "pull_request" ]]; then
   changed_files=$(ah ws cf --range "origin/$GITHUB_BASE_REF...origin/$GITHUB_HEAD_REF" --types ruby)
 else
-  changed_files=$(ah ws cf --range "$GITHUB_SHA" --types ruby)
+  changed_files=$(ah ws cf --range "origin/$GITHUB_REF" --types ruby)
 fi
 
 echo "::group::Changed/Added files"
@@ -14,10 +14,10 @@ if [[ -z "$changed_files" ]]; then
   echo "No changes found, skipping checks"
 else
   case $1 in
-    "style")
+    Style)
       ah m rubocop -- --format github $changed_files -- --only-guide-cops
     ;;
-    "lint")
+    Lint)
       ah m rubocop -- --format github $changed_files -- -l
     ;;
     *)
