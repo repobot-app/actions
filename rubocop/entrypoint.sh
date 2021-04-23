aae416a937f40b5bcc1b7d3fd4e3a3d38bddfcd3
Piotr Solnica <piotr.solnica@gmail.com>
2021-04-23 18:11:42 +0200

    wip

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -5,7 +5,7 @@ git fetch
 if [[ $GITHUB_EVENT_NAME == "pull_request" ]]; then
   changed_files=$(ah ws cf --range "origin/$GITHUB_BASE_REF...origin/$GITHUB_HEAD_REF" --types ruby)
 else
-  changed_files=$(ah ws cf --range "$GITHUB_SHA" --types ruby)
+  changed_files=$(ah ws cf --range "$GITHUB_SHA~1" --types ruby)
 fi
 
 echo "::group::Changed/Added files"
