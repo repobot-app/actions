4e5677385d6f23bd978e64339824f9542e3b92bf
Piotr Solnica <piotr.solnica@gmail.com>
2021-04-23 17:41:26 +0200

    wip

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -8,14 +8,16 @@ else
   changed_files=$(ah ws cf --range "$GITHUB_SHA~1" --types ruby)
 fi
 
-echo "::group::{Changed/Added files}"
-echo "Running against $changed_files"
+echo "::group::Changed/Added files"
+echo "FILES<<STR"
+echo "$changed_files"
+echo "STR"
 echo "::endgroup::"
 
 if [[ -z "$changed_files" ]]; then
   echo "No changes found, skipping checks"
 else
-  echo "::group::{Results}"
+  echo "::group::Results"
   ah m rubocop --root $GITHUB_WORKSPACE -- --format github $changed_files
   echo "::endgroup::"
 fi
