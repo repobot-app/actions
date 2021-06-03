a52835ce5b594421f60a0cc9be77f7ff33cae12f
Piotr Solnica <piotr.solnica@gmail.com>
2021-06-03 15:15:32 +0200

    [rubocop] simplify entrypoint

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -4,26 +4,8 @@ echo "::group::Changed/Added files"
 echo "$GIT_DIFF"
 echo "::endgroup::"
 
-case $1 in
-  Style)
-    ah m rubocop -- --format github $GIT_DIFF -- --only Style --only-recognized-file-types
-  ;;
-  Naming)
-    ah m rubocop -- --format github $GIT_DIFF -- --only Naming --only-recognized-file-types
-  ;;
-  Lint)
-    ah m rubocop -- --format github $GIT_DIFF -- --only Lint --only-recognized-file-types
-  ;;
-  Layout)
-    ah m rubocop -- --format github $GIT_DIFF -- --only Layout --only-recognized-file-types
-  ;;
-  Metrics)
-    ah m rubocop -- --format github $GIT_DIFF -- --only Metrics --only-recognized-file-types
-  ;;
-  Security)
-    ah m rubocop -- --format github $GIT_DIFF -- --only Security --only-recognized-file-types
-  ;;
-  *)
-    ah m rubocop -- --format github $GIT_DIFF -- --only-recognized-file-types
-  ;;
-esac
+if [[ $1 != "" ]]; then
+  ah m rubocop -- --format github --only $1 --only-recognized-file-types
+else
+  ah m rubocop -- --format github --only-recognized-file-types
+fi
