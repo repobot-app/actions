2dfe519dfc878d5d798203fc5f5f2b359671e808
Piotr Solnica <piotr.solnica@gmail.com>
2021-06-03 13:33:37 +0200

    [rubocop] break it down to specific cop groups

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -6,10 +6,22 @@ echo "::endgroup::"
 
 case $1 in
   Style)
-    ah m rubocop -- --format github $GIT_DIFF -- --only-guide-cops --only-recognized-file-types
+    ah m rubocop -- --format github $GIT_DIFF -- --only Style --only-recognized-file-types
+  ;;
+  Naming)
+    ah m rubocop -- --format github $GIT_DIFF -- --only Naming --only-recognized-file-types
   ;;
   Lint)
-    ah m rubocop -- --format github $GIT_DIFF -- -l --only-recognized-file-types
+    ah m rubocop -- --format github $GIT_DIFF -- --only Lint --only-recognized-file-types
+  ;;
+  Layout)
+    ah m rubocop -- --format github $GIT_DIFF -- --only Layout --only-recognized-file-types
+  ;;
+  Metrics)
+    ah m rubocop -- --format github $GIT_DIFF -- --only Metrics --only-recognized-file-types
+  ;;
+  Security)
+    ah m rubocop -- --format github $GIT_DIFF -- --only Security --only-recognized-file-types
   ;;
   *)
     ah m rubocop -- --format github $GIT_DIFF --only-recognized-file-types
