05682bacf7398127781deb52514168df8755bdcd
Piotr Solnica <piotr.solnica@gmail.com>
2021-06-03 15:30:21 +0200

    [rubocop] restore running against changed files

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -5,7 +5,7 @@ echo "$GIT_DIFF"
 echo "::endgroup::"
 
 if [[ $1 != "" ]]; then
-  ah m rubocop -- --format github --only $1 --only-recognized-file-types
+  ah m rubocop -- --format github --only $1 --only-recognized-file-types $GIT_DIFF
 else
-  ah m rubocop -- --format github --only-recognized-file-types
+  ah m rubocop -- --format github --only-recognized-file-types $GIT_DIFF
 fi
