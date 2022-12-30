1b538a3b7a21d6a2158ef9bdb4226b0ca956140a
Peter Solnica <peter@solnica.online>
2022-12-30 11:05:57 +0100

    Aaand fix cli usage for sure this time

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -5,7 +5,7 @@ echo "$2"
 echo "::endgroup::"
 
 if [[ $1 != "" ]]; then
-  rebopot m rubocop -- --format github --only $1 --only-recognized-file-types $2
+  repobot m rubocop -- --format github --only $1 --only-recognized-file-types $2
 else
   repobot m rubocop -- --format github --only-recognized-file-types $2
 fi
